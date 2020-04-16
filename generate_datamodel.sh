39db43cf75e634a3bca9cbd01ba22406df0068e0
Philip Dodds <philip.dodds@me.com>
2020-04-15 20:24:58 -0400

    Lock in version

--- generate_datamodel.sh
+++ generate_datamodel.sh
@@ -2,4 +2,4 @@
 
 
 conda activate kodexa
-datamodel-codegen --input kodexa-swagger.json --output kodexa_cloud/model.py --target-python-version 3.6
+datamodel-codegen --input kodexa-swagger.json --input-file-type jsonschema --output kodexa_cloud/model.py --target-python-version 3.6
