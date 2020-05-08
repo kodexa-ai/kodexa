407704319df1e9c50d94f58a02efa528340f3333
Philip Dodds <philip.dodds@me.com>
2020-05-08 08:38:56 -0400

    Set the output document to the last one through the pipeline

--- kodexa/pipeline/pipeline.py
+++ kodexa/pipeline/pipeline.py
@@ -266,6 +266,7 @@ class Pipeline:
 
             document.log = log_stream.getvalue()
             self.context.statistics.processed_document(document)
+            self.context.output_document = document
 
         logging.info(f"Completed pipeline {self.name}")
 
