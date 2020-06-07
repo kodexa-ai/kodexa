5a51c88500c3d2b5f2fd7f026d04019119a73f79
Philip Dodds <philip.dodds@me.com>
2020-06-07 09:44:07 -0400

    Fix up documentation and imports

--- kodexa/__init__.py
+++ kodexa/__init__.py
@@ -4,7 +4,7 @@
 
 
 """
-from .cloud import KodexaSession, KodexaPipeline, KodexaAction
+from .cloud import KodexaSession, KodexaPipeline, KodexaAction, KodexaPlatform
 from .connectors import FolderConnector, FileHandleConnector, UrlConnector, registered_connectors, get_connectors, \
     get_connector, add_connector, get_source
 from .extractors import *
--- kodexa/cloud/__init__.py
+++ kodexa/cloud/__init__.py
@@ -1,7 +1,7 @@
 """
-Kodexa Cloud
+Kodexa Platform
 ----------
 
-Provides out of the box integration with the Kodexa cloud, enabling the universe of content services that are available
+Provides out of the box integration with the Kodexa paltform, enabling the universe of content services that are available
 """
 from .kodexa import KodexaSession, KodexaPipeline, KodexaAction, KodexaPlatform
