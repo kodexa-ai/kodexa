7122a6fa4794c2769b59f98297f041e6fe87062f
Lecia Pearce <sendtoleca@gmail.com>
2020-06-10 14:06:17 -0400

    adding test to verify pipeline with no steps returns text document

--- tests/basic_pipeline_tests.py
+++ tests/basic_pipeline_tests.py
@@ -234,5 +234,32 @@ def test_table_stores_with_extractor():
 
 def test_basic_url_pipeline():
 
-    pipeline = Pipeline.from_url('http://www.google.com')
+    url = 'http://www.google.com'
+    pipeline = Pipeline.from_url(url)
     pipeline.run()
+
+    doc = pipeline.context.output_document
+    assert doc.metadata.connector_options.url == url
+
+
+
+def test_basic_text_pipeline():
+
+    text = 'The Normans (Norman: Nourmands; French: Normands; Latin: Normanni) ' \
+        'were the people who in the 10th and 11th centuries gave their name to ' \
+        'Normandy, a region in France. They were descended from Norse ' \
+        '(\"Norman\" comes from \"Norseman\") raiders and pirates from Denmark, ' \
+        'Iceland and Norway who, under their leader Rollo, ' \
+        'agreed to swear fealty to King Charles III of West Francia. ' \
+        'Through generations of assimilation and mixing with the native ' \
+        'Frankish and Roman-Gaulish populations, their descendants would gradually ' \
+        'merge with the Carolingian-based cultures of West Francia. ' \
+        'The distinct cultural and ethnic identity of the Normans emerged initially ' \
+        'in the first half of the 10th century, ' \
+        'and it continued to evolve over the succeeding centuries.' \
+
+    pipeline = Pipeline.from_text(text)
+    pipeline.run()
+    doc = pipeline.context.output_document
+
+    assert len(doc.get_root().get_all_content()) == 742
