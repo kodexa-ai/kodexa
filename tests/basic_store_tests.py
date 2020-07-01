06695cec462cabd8e5662262ac929e400ae46e6f
Lecia Pearce <sendtoleca@gmail.com>
2020-07-01 11:30:19 -0400

    adding more tests for TagsToKeyValuePairExtractor

--- tests/basic_store_tests.py
+++ tests/basic_store_tests.py
@@ -1,7 +1,11 @@
 import shutil
+import os
 
-from kodexa import JsonDocumentStore
-from kodexa.model import DocumentMetadata, Document
+from kodexa import Document, DocumentRender, Pipeline, PipelineContext, DocumentMetadata, JsonDocumentStore, TagsToKeyValuePairExtractor, NodeTagger
+
+
+def get_test_directory():
+    return os.path.dirname(os.path.abspath(__file__)) + "/../test_documents/"
 
 
 def get_test_document():
@@ -25,3 +29,27 @@ def test_basic_json_store():
 
     store2 = JsonDocumentStore(store_path='/tmp/json-store')
     assert store2.count() == 2
+
+def test_table_data_store():
+	# Testing with 'include_node_content' set to True.  Should result in 3 columns
+	pipeline = Pipeline(Document.from_kdxa(os.path.join(get_test_directory(), 'tongue_twister.kdxa')))
+	pipeline.add_step(NodeTagger(selector='//*[contentRegex(".*flue.*")]', tag_to_apply='has_flue', node_only=True))
+	pipeline.add_step(TagsToKeyValuePairExtractor(store_name='tagged_data', include_node_content=True))
+	pipeline.run()
+
+	tagged_pairs_store = pipeline.context.get_store('tagged_data')
+
+	assert len(tagged_pairs_store.columns) == 3
+	assert len(tagged_pairs_store.rows) == 2
+
+
+	# Testing with 'include_node_content' set to False.  Should result in 2 columns
+	pipeline2 = Pipeline(Document.from_kdxa(os.path.join(get_test_directory(), 'tongue_twister.kdxa')))
+	pipeline2.add_step(NodeTagger(selector='//*[contentRegex(".*flue.*")]', tag_to_apply='has_flue', node_only=True))
+	pipeline2.add_step(TagsToKeyValuePairExtractor(store_name='tagged_data_2', include_node_content=False))
+	pipeline2.run()
+
+	tagged_pairs_store2 = pipeline2.context.get_store('tagged_data_2')
+
+	assert len(tagged_pairs_store2.columns) == 2
+	assert len(tagged_pairs_store2.rows) == 2
