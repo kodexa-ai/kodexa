b977e550d01369d9ab866cf5a023ebda36014f8a
Lecia Pearce <sendtoleca@gmail.com>
2020-07-08 08:16:10 -0400

    documentation updates for core mixins

--- kodexa/mixins/core.py
+++ kodexa/mixins/core.py
@@ -28,20 +28,28 @@ def update_content_markup(self):
 def find(self, content_re=".*", type_re=".*", direction=FindDirection.CHILDREN, tag_name=None, instance=0,
          tag_name_re=None, use_all_content=False):
     """
-    Search for a node that matches on the value and or type using regular expressions
+    Return a node related to this node (parent or child) that matches the content and/or type specified by regular expressions.
 
-        >>> document.content_node.find(content_re='.*Cheese.*',instance=2)
+        >>> document.get_root().find(content_re='.*Cheese.*',instance=2)
         <kodexa.model.model.ContentNode object at 0x7f80605e53c8>
 
-    :param content_re: the regular expression to match the nodes content (default '.*')
-    :param type_re: the regular expression to match the nodes type (default '.*')
-    :param direction: the direction to search, either FindDirection.CHILDREN or FindDirection.PARENT (default CHILDREN)
-    :param tag_name: the tag name that must exist
-    :param instance: the instance to return (0=first)
-    :param tag_name_re: the regular expression to match for the tag_name
-    :param use_all_content: match the content for all child nodes to (default False)
+    :param content_re: The regular expression to match against the node's content; default is '.*'.
+    :type content_re: str, optional
+    :param type_re: The regular expression to match against the node's type; default is '.*'.
+    :type type_re: str, optional
+    :param direction: The direction to search (CHILDREN or PARENT); default is FindDirection.CHILDREN.
+    :type direction: FindDirection(enum), optional
+    :param tag_name: The tag name that must exist on the node; default is None.
+    :type tag_name: str, optional
+    :param instance: The instance of the matching node to return (may have multiple matches); default is 0.
+    :type instance: int, optional
+    :param tag_name_re: The regular expression that will match the tag_name that must exist on the node;  default is None.
+    :type tag_name_re: str, optional
+    :param use_all_content: Match content_re against the content of this node concatenated with the content of its child nodes; default is False.
+    :type use_all_content: bool, optional
 
-    :return: either an instance of ContentNode (if found), or None
+    :return: Matching node (if found), or None.
+    :rtype: ContentNode or None.
     """
     results = self.findall(content_re, type_re, direction, tag_name, tag_name_re, use_all_content)
     if len(results) < instance + 1:
@@ -50,39 +58,44 @@ def find(self, content_re=".*", type_re=".*", direction=FindDirection.CHILDREN, 
         return results[instance]
 
 
-def find_with_feature_value(self, feature_type, feature, value, direction=FindDirection.CHILDREN, instance=1):
+def find_with_feature_value(self, feature_type, feature_name, value, direction=FindDirection.CHILDREN, instance=1):
     """
-    Search for a node with a specific feature type, name and value
+    Return a node related to this node (parent or child) that has a specific feature type, feature name, and feature value.
 
-        >>> document.content_node.find_with_feature_value(feature_type='tag',feature='is_cheese',value=[1,10,'The Cheese has moved'])
+        >>> document.content_node.find_with_feature_value(feature_type='tag',feature_name='is_cheese',value=[1,10,'The Cheese has moved'])
         <kodexa.model.model.ContentNode object at 0x7f80605e53c8>
 
-    :param feature_type: the feature type
-    :param feature: the feature name
-    :param value: the feature value
-    :param direction: the direction to search, either FindDirection.CHILDREN or FindDirection.PARENT (default CHILDREN)
-    :param instance: the instance to get (defaul 1)
+    :param str feature_type: The feature type.
+    :param str feature_name: The feature name.
+    :param Any value: The feature value.
+    :param direction: The direction to search (CHILDREN or PARENT); default is FindDirection.CHILDREN.
+    :type direction: FindDirection(enum), optional
+    :param instance: The instance of the matching node to return (may have multiple matches); default is 1.
+    :type instance: int, optional
 
-    :return: either an instance of ContentNode (if found), or None
+    :return: Matching node (if found), or None.
+    :rtype: ContentNode or None
     """
     return next(
-        itertools.islice(self.findall_with_feature_value(feature_type, feature, value, direction), instance - 1, 1),
+        itertools.islice(self.findall_with_feature_value(feature_type, feature_name, value, direction), instance - 1, 1),
         None)
 
 
 def findall_with_feature_value(self, feature_type, feature_name, value, direction=FindDirection.CHILDREN):
     """
-    Search for all nodes with a specific feature type, name and value
+    Get all nodes related to this node (parents or children) that have a specific feature type, feature name, and feature value.
 
         >>> document.content_node.findall_with_feature_value(feature_type='tag',feature='is_cheese', value=[1,10,'The Cheese has moved'])
         [<kodexa.model.model.ContentNode object at 0x7f80605e53c8>]
 
-    :param feature_type: the feature type
-    :param feature_name: the feature name
-    :param value: the feature value
-    :param direction: the direction to search, either FindDirection.CHILDREN or FindDirection.PARENT (default CHILDREN)
+    :param str feature_type: The feature type.
+    :param str feature_name: The feature name.
+    :param Any value: The feature value.
+    :param direction: The direction to search (CHILDREN or PARENT); default is FindDirection.CHILDREN.
+    :type direction: FindDirection(enum), optional
 
     :return: list of the matching content nodes
+    :rtype: list[ContentNode]
     """
     if self.has_feature(feature_type, feature_name) and value == self.get_feature_value(feature_type, feature_name):
         yield self
@@ -97,11 +110,11 @@ def findall_with_feature_value(self, feature_type, feature_name, value, directio
 
 def is_first_child(self):
     """
-    Returns True if this is the first child, also True if it has no parent
-
-    :return: True if this is the first child
+    Determines if this node is the first child of its parent or has no parent.
+    
+    :return: True if this node is the first child of its parent or if this node has no parent; else, False;
+    :rtype: bool
     """
-
     if not self.parent:
         return True
     else:
@@ -110,9 +123,10 @@ def is_first_child(self):
 
 def is_last_child(self):
     """
-    Returns True if this is the last child, also True if it has no parent
+    Determines if this node is the last child of its parent or has no parent.
 
-    :return: True if this is the first child
+    :return: True if this node is the last child of its parent or if this node has no parent; else, False;
+    :rtype: bool
     """
 
     if not self.parent:
@@ -123,10 +137,10 @@ def is_last_child(self):
 
 def get_last_child_index(self):
     """
-    Returns the max index value for the children of this node, if the node has no children
-    it returns None
+    Returns the max index value for the children of this node. If the node has no children, returns None.
 
-    :return: Thhe max index of the children of this node, or None is no children
+    :return: The max index of the children of this node, or None if there are no children.
+    :rtype: int or None
     """
 
     if not self.children:
@@ -142,14 +156,17 @@ def get_last_child_index(self):
 
 def get_node_at_index(self, index):
     """
-    Returns the node at a specific index,  if the index it outside the first (0), or
-    last index it will return null, if not it will return the node at index, if there
-    isn't a node at that index it will return a 'virtual' node that will represent
-    the node to the side of this node and have an index and no features or content
+    Returns the child node at the specified index. If the specified index is outside the first (0), or
+    last child's index, None is returned.  
+    
+    Note:  documents allow for sparse representation and child nodes may not have consecutive index numbers. 
+    If there isn't a child node at the specfied index, a 'virtual' node will be returned.  This 'virtual' node
+    will have the type of its nearest sibling and will have an index value, but will have no features or content.
 
-    :param index: The index (zero-based) for the child node
+    :param int index: The index (zero-based) for the child node.
 
-    :return: Node at index, or None is the index is outside the boundaries of child nodes
+    :return: Node at index, or None if the index is outside the boundaries of child nodes.
+    :rtype: ContentNode or None
     """
     if self.children:
 
@@ -180,40 +197,51 @@ def get_node_at_index(self, index):
 
 def has_next_node(self, type_re=".*", skip_virtual=False):
     """
-    Returns True if the node has a next node
+    Determine if this node has a next sibling that matches the type specified by the type_re regex.
 
-    :param type_re: Type name (regular expression)
-    :param skip_virtual: True to skip any virtual nodes and only return the next real node (default False)
+    :param type_re: The regular expression to match against the next sibling node's type; default is '.*'.
+    :type type_re: str, optional
+    :param skip_virtual: Skip virtual nodes and return the next real node; default is False.
+    :type skip_virtual: bool, optional
 
-    :return: True if there is a next sibling node
+    :return: True if there is a next sibling node matching the specified type regex; else, False.
+    :rtype: bool
     """
     return self.next_node(type_re, skip_virtual=skip_virtual) is not None
 
 
 def has_previous_node(self, type_re=".*", skip_virtual=False):
     """
-     Returns True if the node has a previous node
+    Determine if this node has a previous sibling that matches the type specified by the type_re regex.
 
-    :param type_re: Type name (regular expression)
-    :param skip_virtual: True to skip any virtual nodes and only return the next real node (default False)
+    :param type_re: The regular expression to match against the previous sibling node's type; default is '.*'.
+    :type type_re: str, optional
+    :param skip_virtual: Skip virtual nodes and return the next real node; default is False.
+    :type skip_virtual: bool, optional
 
-     :return: True if there is a previous sibling node
+    :return: True if there is a previous sibling node matching the specified type regex; else, False.
+    :rtype: bool
      """
     return self.previous_node(type_re=type_re, skip_virtual=skip_virtual) is not None
 
 
-def next_node(self, type_re='.*', skip_virtual=False, has_no_content=False, traverse=Traverse.SIBLING):
+def next_node(self, type_re='.*', skip_virtual=False, has_no_content=False):
     """
-    Returns the next sibling content node, note that this logic is based on the index,
-    therefore the next node might actually be a virtual node that is created to fill a gap
-    in the document,  since the index allows for sparse documents
+    Returns the next sibling content node. 
+    
+    Note:  This logic relies on node indexes.  Documents allow for sparse representation and child nodes may not have consecutive index numbers.
+    Therefore, the next node might actually be a virtual node that is created to fill a gap in the document.  You can skip virtual nodes by setting the 
+    skip_virtual parameter to False.
 
-    :param type_re: the regular expression for the type of node (default '.*')
-    :param skip_virtual: True to skip any virtual nodes and only return the next real node (default False)
-    :param has_no_content: True if you only want to return a node that has no content
-    :param traverse: By default we traverse siblings, however you can include CHILDREN, PARENT or ALL
+    :param type_re: The regular expression to match against the next sibling node's type; default is '.*'.
+    :type type_re: str, optional
+    :param skip_virtual: Skip virtual nodes and return the next real node; default is False.
+    :type skip_virtual: bool, optional
+    :param has_no_content: Allow a node that has no content to be returned; default is False.
+    :type has_no_content: bool, optional
 
-    :return: the next node or None if no node exists
+    :return: The next node or None, if no node exists
+    :rtype: ContentNode or None
     """
     search_index = self.index + 1
     compiled_type_re = re.compile(type_re)
@@ -234,16 +262,23 @@ def next_node(self, type_re='.*', skip_virtual=False, has_no_content=False, trav
 
 def previous_node(self, type_re='.*', skip_virtual=False, has_no_content=False, traverse=Traverse.SIBLING):
     """
-    Returns the previous sibling content node, note that this logic is based on the index,
-    therefore the next node might actually be a virtual node that is created to fill a gap
-    in the document,  since the index allows for sparse documents
+    Returns the previous sibling content node. 
+    
+    Note:  This logic relies on node indexes.  Documents allow for sparse representation and child nodes may not have consecutive index numbers.
+    Therefore, the previous node might actually be a virtual node that is created to fill a gap in the document.  You can skip virtual nodes by setting the 
+    skip_virtual parameter to False.
 
-    :param type_re: the regular expression for the type of node (default '.*')
-    :param skip_virtual: True to skip any virtual nodes and only return the next real node (default False)
-    :param has_no_content: True if you only want to return a node that no content
-    :param traverse: By default we traverse siblings, however you can include CHILDREN, PARENT or ALL
+    :param type_re: The regular expression to match against the previous node's type; default is '.*'.
+    :type type_re: str, optional
+    :param skip_virtual: Skip virtual nodes and return the next real node; default is False.
+    :type skip_virtual: bool, optional
+    :param has_no_content: Allow a node that has no content to be returned; default is False.
+    :type has_no_content: bool, optional
+    :param traverse: The relationship you'd like to traverse (SIBLING, CHILDREN, PARENT, or ALL); default is Traverse.SIBLING.
+    :type traverse: Traverse(enum), optional
 
-    :return: the previous node or None if no node exists
+    :return: The previous node or None, if no node exists
+    :rtype: ContentNode or None
     """
     if self.index == 0:
         if traverse == traverse.ALL or traverse == traverse.PARENT and self.parent:
@@ -271,22 +306,27 @@ def previous_node(self, type_re='.*', skip_virtual=False, has_no_content=False, 
 def findall(self, content_re=".*", type_re=".*", direction=FindDirection.CHILDREN, tag_name=None,
             tag_name_re=None, use_all_content=False):
     """
-    Search for nodes that matches on the value and or type using regular expressions
+    Search for related nodes (child or parent) that match the content and/or type specified by regular expressions.
 
         >>> document.content_node.findall(content_re='.*Cheese.*')
         [<kodexa.model.model.ContentNode object at 0x7f80605e53c8>,
         <kodexa.model.model.ContentNode object at 0x7f80605e53c8>]
 
-    :param content_re: the regular expression to match the nodes content (default '.*')
-    :param type_re: the regular expression to match the nodes type (default '.*')
-    :param direction: the direction to search, either FindDirection.CHILDREN or FindDirection.PARENT (default CHILDREN)
-    :param tag_name: the tag name that must exist
-    :param tag_name_re: the regular expression to match for the tag_name
-    :param use_all_content: match the content for all child nodes to (default False)
+    :param content_re: The regular expression to match against the node's content; default is '.*'.
+    :type content_re: str, optional
+    :param type_re: The regular expression to match against the node's type; default is '.*'.
+    :type type_re: str, optional
+    :param direction: The direction to search (CHILDREN or PARENT); default is FindDirection.CHILDREN.
+    :type direction: FindDirection(enum), optional
+    :param tag_name: The tag name that must exist on the node; default is None.
+    :type tag_name: str, optional
+    :param tag_name_re: The regular expression that will match the tag_name that must exist on the node;  default is None.
+    :type tag_name_re: str, optional
+    :param use_all_content: Match content_re against the content of this node concatenated with the content of its child nodes; default is False.
+    :type use_all_content: bool, optional
 
-    :return: list of matching content nodes
-
-
+    :return: List of matching content nodes
+    :rtype: list[ContentNode]
     """
     value_compiled = re.compile(content_re)
     type_compiled = re.compile(type_re)
