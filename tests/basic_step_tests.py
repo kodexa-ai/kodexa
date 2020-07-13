f7ca7d941b6adc1823761eb37bea2cc8c29b9571
Lecia Pearce <sendtoleca@gmail.com>
2020-07-13 15:55:11 -0400

    fixing bug in RollupTransformer

--- tests/basic_step_tests.py
+++ tests/basic_step_tests.py
@@ -1,6 +1,6 @@
 import os
 
-from kodexa import Document, DocumentRender, PipelineContext, TagsToKeyValuePairExtractor, RollupTransformer
+from kodexa import Document, DocumentRender, Pipeline, PipelineContext, TagsToKeyValuePairExtractor, RollupTransformer
 
 
 def get_test_directory():
@@ -35,3 +35,33 @@ def test_tag_key_value():
     assert context.get_store('test_store').count() == 45
     assert context.get_store('test_store').rows[14][0] == 'LOC'
     assert context.get_store('test_store').rows[14][1] == 'Europe'
+
+def test_rollup_of_pdf():
+
+    # first test - collapsing words and lines up to their common parent
+    test_doc = Document.from_kdxa(get_test_directory() + '20200709loanboss.kdxa')
+
+    rollup_pipeline = Pipeline(test_doc)
+    rollup_pipeline.add_step(RollupTransformer(collapse_type_res=["word", "line"]))
+    rollup_pipeline.run()
+
+    collapsed_doc = rollup_pipeline.context.output_document
+
+    ## This will collapse all word nodes into lines and then lines into content-areas, but there are no spaces retained
+    ## all the text rolls together
+    print(collapsed_doc.select("//content-area")[12].get_all_content())
+    #'follows:aswarrantandrepresentagree,covenant,herebyheretopartiestheAgreement,thisinforthsetwarrantiesandrepresentationsagreements,covenants,theandLenderbyLoantheofmakingtheofconsiderationinTHEREFORE,NOW'
+
+
+    ## second test - just collapse the line up to its parent (content-area)
+    test_doc = Document.from_kdxa(get_test_directory() + '20200709loanboss.kdxa')
+
+    rollup_pipeline = Pipeline(test_doc)
+    rollup_pipeline.add_step(RollupTransformer(collapse_type_res=["line"]))
+    rollup_pipeline.run()
+
+    collapsed_doc = rollup_pipeline.context.output_document
+
+    ## there is no text on the content-area because we're not getting all content for the node we're collapsing
+    ## and the line had no content of it's own, it was it's children
+    print(collapsed_doc.select("//content-area")[12].get_all_content())
