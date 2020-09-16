7468d6c163965a5eddfe774e3dd8c62cb505a3a1
Philip Dodds <philip.dodds@me.com>
2020-09-16 09:48:46 -0400

    New tagging capabilities for tagging child nodes with fixed position and offset

--- tests/test_snapshots/tagging_tests.py
+++ tests/test_snapshots/tagging_tests.py
@@ -12,6 +12,14 @@ def test_fixed_tagging_with_child():
     doc = Document.from_text("Hello")
     doc.content_node.add_child_content("text", "Philip")
     doc.content_node.add_child_content("text", "Dodds")
-    doc.content_node.tag('name', fixed_position=[6, 12])
+
+    #Hello Philip Dodds
+    #012345678901234567
+
+    doc.content_node.tag('name', fixed_position=[6, 11], separator=" ")
+
+    doc.content_node.tag('lastName', fixed_position=[13, 17], separator=" ")
+    print(doc.content_node.tag_text_tree())
 
     assert doc.content_node.get_tag_values('name', include_children=True)[0] == 'Philip'
+    assert doc.content_node.get_tag_values('lastName', include_children=True)[0] == 'Dodds'
