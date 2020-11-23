02d106cfcc8d031010d4dae37ca706ef9103047b
Philip Dodds <philip@kodexa.com>
2020-11-23 05:36:28 -0500

    More work on the model and new test cases

--- kodexa/stores/__init__.py
+++ kodexa/stores/__init__.py
@@ -5,4 +5,4 @@ Stores
 Stores are persistence components for Documents.  Typically, they can act as either a Connector or a Sink
 """
 from .stores import JsonDocumentStore, TableDataStore, DictDataStore, DataStoreHelper, LocalDocumentStore, \
-    RemoteDocumentStore, RemoteDictDataStore, RemoteTableDataStore
+    RemoteDocumentStore, RemoteDictDataStore, RemoteTableDataStore, RemoteModelStore, LocalModelStore
