bde88970955ab4f6f1384f4b0524dcf416f179be
Philip Dodds <philip@kodexa.com>
2020-11-29 06:38:54 -0500

    Include label on the taxon

--- kodexa/taxonomy/taxonomy.py
+++ kodexa/taxonomy/taxonomy.py
@@ -1,11 +1,13 @@
 import random
-from typing import List
+from typing import List, Optional
 
 
 class Taxon:
 
-    def __init__(self, name: str, color: str = None):
+    def __init__(self, label: str, name: str, id: Optional[str] = None, color: Optional[str] = None):
+        self.id = id
         self.name: str = name
+        self.label: str = label
         self.color = "#" + ("%06x" % random.randint(0, 0xFFFFFF)) if color is None else color
         self.children: List[Taxon] = []
 
