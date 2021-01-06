2e2dd2e5c2d6227bd3b21dd38868ae845ffc44f7
Philip Dodds <philip@kodexa.com>
2021-01-06 11:29:59 -0500

    Ability to setup process taxonomies and taxonomy types

--- kodexa/assistant/assistant.py
+++ kodexa/assistant/assistant.py
@@ -3,6 +3,8 @@ class Assistant:
     def __init__(self, full_description=None):
         self.required_stores = []
         self.workflows = []
+        self.processing_taxonomies = []
+
         self.full_description = full_description
 
     def add_store(self, name: str, title: str, description: str, store_type: str):
@@ -17,4 +19,9 @@ class Assistant:
         self.workflows.append({
             'ref': ref,
             'parameters': parameters
+        })
+
+    def add_processing_taxonomy(self, ref: str):
+        self.processing_taxonomies.append({
+            'ref': ref
         })
