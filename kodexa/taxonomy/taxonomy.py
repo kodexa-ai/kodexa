960d9dd48a5ac368fe8d8d70d01b3c1a4a9a32ab
Philip Dodds <philip@kodexa.com>
2021-01-13 14:14:10 -0500

    Adding a little more support for assistants and also remote taxonomies

--- kodexa/taxonomy/taxonomy.py
+++ kodexa/taxonomy/taxonomy.py
@@ -21,3 +21,32 @@ class Taxonomy:
 
     def add_taxon(self, label: str, name: str):
         self.taxons.append(Taxon(label, name))
+
+
+class RemoteTaxonomy:
+
+    def __init__(self, ref: str):
+        from kodexa import KodexaPlatform
+        url = f"{KodexaPlatform.get_url()}/api/taxonomies/{ref.replace(':', '/')}"
+
+        import requests
+        response = requests.get(url,
+                                headers={"x-access-token": KodexaPlatform.get_access_token(),
+                                         "content-type": "application/json"})
+
+        self.taxonomy_type = response.json()['taxonomy_type']
+        self.enabled = response.json()['enabled']
+
+        self.taxons = []
+
+        def build_taxons(json_taxons, taxons):
+            for json_taxon in json_taxons:
+                new_taxon = Taxon(label=json_taxon['label'], name=json_taxon['name'], id=json_taxon['id'],
+                                  color=json_taxon['color'])
+
+                if 'children' in json_taxon:
+                    build_taxons(json_taxon['children'], new_taxon.children)
+
+                taxons.append(new_taxon)
+
+        build_taxons(response.json()['taxons'], self.taxons)
