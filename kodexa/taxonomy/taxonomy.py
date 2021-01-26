3bbe6cb72a176536fc2aa0ac77413dbed30797d1
Philip Dodds <philip@kodexa.com>
2021-01-26 14:18:28 -0500

    Merged screw up

--- kodexa/taxonomy/taxonomy.py
+++ kodexa/taxonomy/taxonomy.py
@@ -1,12 +1,17 @@
 import random
 from typing import List, Optional
-
+import uuid
 
 class Taxon:
 
     def __init__(self, label: str, name: str, id: Optional[str] = None, color: Optional[str] = None,
-                 value_path: Optional[str] = None, data_path: Optional[str] = None, metadata_path: Optional[str] = None):
-        self.id = id
+                 value_path: Optional[str] = None, data_path: Optional[str] = None, options=None,
+                 metadata_path: Optional[str] = None, node_types=None, enabled=True):
+        if options is None:
+            options = []
+        if node_types is None:
+            node_types = []
+        self.id = id if id and len(id) > 0 else str(uuid.uuid4())
         self.name: str = name
         self.label: str = label
         self.color = "#" + ("%06x" % random.randint(0, 0xFFFFFF)) if color is None else color
@@ -14,6 +19,34 @@ class Taxon:
         self.value_path = value_path
         self.data_path = data_path
         self.metadata_path = metadata_path
+        self.node_types = node_types
+        self.options = options
+        self.enabled = enabled
+
+    @staticmethod
+    def from_dict(dict_taxon):
+        new_taxon = Taxon(label=dict_taxon['label'], name=dict_taxon['name'], id=dict_taxon['id'],
+                          color=dict_taxon['color'], node_types=dict_taxon['nodeTypes'],
+                          options=dict_taxon['options'], enabled=self.enabled,
+                          value_path=dict_taxon['valuePath'] if 'valuePath' in dict_taxon else None,
+                          data_path=dict_taxon['dataPath'] if 'dataPath' in dict_taxon else None,
+                          metadata_path=dict_taxon['metadataPath'] if 'metadataPath' in dict_taxon else None)
+
+        if 'children' in dict_taxon:
+            for child_taxon_dict in dict_taxon['children']:
+                new_taxon.children.append(Taxon.from_dict(child_taxon_dict))
+
+        return new_taxon
+
+    def to_dict(self):
+        dict = {'id': self.id, 'name': self.name, 'color': self.color, 'valuePath': self.value_path,
+                'label': self.label, 'nodeTypes': self.node_types, 'options': self.options, 'enabled': self.enabled,
+                'dataPath': self.data_path, 'metadataPath': self.metadata_path, 'children': []}
+
+        for child in self.children:
+            dict['children'].append(child.to_dict())
+
+        return dict
 
 
 class Taxonomy:
@@ -29,10 +62,30 @@ class Taxonomy:
         self.taxons.append(new_taxon)
         return new_taxon
 
+    @staticmethod
+    def from_dict(dict_taxonomy):
+        new_taxonomy = Taxonomy(ref=dict_taxonomy['ref'], enabled=dict_taxonomy['enabled'],
+                                taxonomy_type=dict_taxonomy['taxonomyType'])
+
+        if 'taxons' in dict_taxonomy:
+            for taxon_dict in dict_taxonomy['taxons']:
+                new_taxonomy.taxons.append(Taxon.from_dict(taxon_dict))
+
+        return new_taxonomy
+
+    def to_dict(self):
+        dict = {'taxonomyType': self.taxonomy_type, 'enabled': self.enabled, 'ref': self.ref, 'taxons': []}
+
+        for child in self.taxons:
+            dict['taxons'].append(child.to_dict())
+
+        return dict
+
 
 class RemoteTaxonomy:
 
-    def __init__(self, ref: str):
+    @staticmethod
+    def get(ref: str):
         from kodexa import KodexaPlatform
         url = f"{KodexaPlatform.get_url()}/api/taxonomies/{ref.replace(':', '/')}"
 
@@ -41,24 +94,4 @@ class RemoteTaxonomy:
                                 headers={"x-access-token": KodexaPlatform.get_access_token(),
                                          "content-type": "application/json"})
 
-        self.taxonomy_type = response.json()['taxonomyType']
-        self.enabled = response.json()['enabled']
-        self.ref = ref
-        self.name = response.json()['name']
-
-        self.taxons: List[Taxon] = []
-
-        def build_taxons(json_taxons, taxons):
-            for json_taxon in json_taxons:
-                new_taxon = Taxon(label=json_taxon['label'], name=json_taxon['name'], id=json_taxon['id'],
-                                  color=json_taxon['color'],
-                                  value_path=json_taxon['valuePath'] if 'valuePath' in json_taxon else None,
-                                  data_path=json_taxon['dataPath'] if 'dataPath' in json_taxon else None,
-                                  metadata_path=json_taxon['metadataPath'] if 'metadataPath' in json_taxon else None)
-
-                if 'children' in json_taxon:
-                    build_taxons(json_taxon['children'], new_taxon.children)
-
-                taxons.append(new_taxon)
-
-        build_taxons(response.json()['taxons'], self.taxons)
+        return Taxonomy.from_dict()
