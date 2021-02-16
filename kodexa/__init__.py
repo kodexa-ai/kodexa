4fa0af290a7ff9fcb11d2e4e396ae1d78bd38d52
Philip Dodds <philip@kodexa.com>
2021-02-16 09:01:23 -0500

    Fix up the imports

--- kodexa/__init__.py
+++ kodexa/__init__.py
@@ -2,6 +2,7 @@
     Kodexa is a Python framework to enable flexible data engineering with semi-structured and unstructured documents and
     data.
 """
+from .assistant import Assistant
 from .cloud import RemoteSession, RemotePipeline, RemoteAction, KodexaPlatform
 from .connectors import FolderConnector, FileHandleConnector, UrlConnector, registered_connectors, get_connectors, \
     get_connector, add_connector, get_source
@@ -11,8 +12,7 @@ from .model import DocumentMetadata, ContentNode, ContentFeature, SourceMetadata
 from .pipeline import Pipeline, PipelineStatistics, PipelineContext
 from .sinks import InMemoryDocumentSink, FolderSink
 from .steps import NodeTagger, NodeTagCopy, TextParser, RollupTransformer, TagsToKeyValuePairExtractor
-from .stores import JsonDocumentStore, TableDataStore, DictDataStore, DataStoreHelper, LocalDocumentStore, \
+from .stores import TableDataStore, DataStoreHelper, LocalDocumentStore, \
     RemoteDocumentStore, RemoteTableDataStore, RemoteDictDataStore, RemoteModelStore, LocalModelStore
 from .taxonomy import Taxonomy, Taxon, RemoteTaxonomy
 from .workflow import Workflow, CronSchedule, WebSchedule, StorePublisher
-from .assistant import Assistant
