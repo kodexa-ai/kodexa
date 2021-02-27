890f48d4771195a04dc7ec40c49646e0577885d4
Philip Dodds <philip@kodexa.com>
2021-02-27 11:03:07 -0500

    Fix up the connector so we have the family for the get

---
+++ kodexa/model/document.py
@@ -0,0 +1,13 @@
+from kodexa import Document
+
+
+class DocumentV3(Document):
+    """
+    The v3 implementation of a Kodexa document which is
+    designed for high-performance and very large documents and uses
+    an underlying SQLLite database to manage the storage of the
+    content structure and features
+
+    Work in Progress
+    """
+    pass
