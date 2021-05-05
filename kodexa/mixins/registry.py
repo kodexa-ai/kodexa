0e5a51d6f24f4e698e4c6badaba0c2e785f9f81c
Philip Dodds <philip@kodexa.com>
2021-05-05 18:24:35 -0400

    Fix typo

--- kodexa/mixins/registry.py
+++ kodexa/mixins/registry.py
@@ -95,7 +95,7 @@ def add_mixin_to_document(mixin, document):
 
         if mixin not in registered_mixins or document.disable_mixin_methods:
             return
-)
+
         if document.content_node and document.mixin:
             apply_to_node(mixin, document.content_node)
 
