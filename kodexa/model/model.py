92c667e641c4bac5d2c161fca868c98db8ac71b6
Philip Dodds <philip@kodexa.ai>
2021-06-18 07:18:28 -0400

    Include additional tagging test

--- kodexa/model/model.py
+++ kodexa/model/model.py
@@ -1216,7 +1216,7 @@ class ContentNode(object):
                     if result < 0 or (end - result) < 0:
                         return -1
                     else:
-                        content_length = content_length + result + len(separator)
+                        content_length = content_length + result
                         end = end - result + len(separator)
                         start = 0 if start - result < 0 else start - (result+len(separator))
 
