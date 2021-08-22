69991a40a1e1a40d2c22130ab5b620e81df52193
Philip Dodds <philip.dodds@me.com>
2021-08-22 17:11:18 -0400

    Bump python version

--- generate-client-stub.sh
+++ generate-client-stub.sh
@@ -1,3 +1,3 @@
-openapi-generator generate -i https://lehua.kodexa.ai/v3/api-docs -g python -o /tmp/client -p packageName=kodexa.client
+openapi-generator generate -i https://lehua.kodexa.ai/v3/api-docs -g python -o /tmp/client -p packageName=kodexa.client -p generateSourceCodeOnly=true -p
 rm -rf kodexa/client
 cp -r /tmp/client/kodexa/client kodexa/.
