550dde7176777a21f4b30b4518939b14ea16e03b
Philip Dodds <philip.dodds@me.com>
2021-08-23 07:50:28 -0400

    Starting to work on the ANTLR integration for 4.0

--- generate-client-stub.sh
+++ generate-client-stub.sh
@@ -1,3 +1,7 @@
-openapi-generator generate -i https://lehua.kodexa.ai/v3/api-docs -g python -o /tmp/client -p packageName=kodexa.client -p generateSourceCodeOnly=true -p
+openapi-generator generate -i https://lehua.kodexa.ai/v3/api-docs -g python -o /tmp/client -p packageName=kodexa.client -p generateSourceCodeOnly=true
 rm -rf kodexa/client
 cp -r /tmp/client/kodexa/client kodexa/.
+rm -rf kodexa/client/docs
+rm -rf kodexa/client/test
+
+antlr -Dlanguage=Python3 resources/selector.g4 -o kodexa/selectors
