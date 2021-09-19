76354801cc9771c1e8dfc67caeee4fadcf38589f
Philip Dodds <philip@kodexa.ai>
2021-09-19 16:13:22 -0400

    Putting the UUID in place

--- generate-client-stub.sh
+++ generate-client-stub.sh
@@ -1,4 +1,4 @@
-openapi-generator generate -i https://lehua.kodexa.ai/v3/api-docs -g python -o /tmp/client -p packageName=kodexa.client -p generateSourceCodeOnly=true
+openapi-generator generate -i resources/api.yaml -g python -o /tmp/client -p packageName=kodexa.client -p generateSourceCodeOnly=true
 rm -rf kodexa/client
 cp -r /tmp/client/kodexa/client kodexa/.
 rm -rf kodexa/client/docs
