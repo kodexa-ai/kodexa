75818e2bb85d1b7f7b39368af1c22c0522333df4
Philip Dodds <philip@kodexa.ai>
2021-09-20 04:28:34 -0400

    Make sure that closing a database is closing the connections as well

--- generate-client-stub.sh
+++ generate-client-stub.sh
@@ -1,7 +1,1 @@
-openapi-generator generate -i resources/api.yaml -g python -o /tmp/client -p packageName=kodexa.client -p generateSourceCodeOnly=true
-rm -rf kodexa/client
-cp -r /tmp/client/kodexa/client kodexa/.
-rm -rf kodexa/client/docs
-rm -rf kodexa/client/test
-
 antlr -Dlanguage=Python3 resources/selector.g4 -o kodexa/selectors
