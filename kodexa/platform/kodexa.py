107c15322539370cabe4fac26bf5fb7bc2e52fe7
Philip Dodds <philip@kodexa.ai>
2021-10-25 07:40:52 -0400

    Fix up the get_object_instance calls

--- kodexa/platform/kodexa.py
+++ kodexa/platform/kodexa.py
@@ -414,17 +414,18 @@ class KodexaPlatform:
     def get_object_instance(ref: str, object_type):
         object_type, object_type_metadata = resolve_object_type(object_type)
 
-        if object_type_metadata.type is Taxonomy:
-            KodexaPlatform.__build_object(ref, object_type_metadata, Taxonomy)
+        if object_type_metadata['type'] is Taxonomy:
+            return KodexaPlatform.__build_object(ref, object_type_metadata, Taxonomy)
+
         if object_type == 'stores':
             # We need to work out what type of store we have
             obj_info = KodexaPlatform.get_object(ref, object_type)
             if obj_info['storeType'] == 'TABLE':
                 return KodexaPlatform.__build_object(ref, object_type_metadata, RemoteDataStore)
             if obj_info['storeType'] == 'DOCUMENT':
-                KodexaPlatform.__build_object(ref, object_type_metadata, RemoteDocumentStore)
+                return KodexaPlatform.__build_object(ref, object_type_metadata, RemoteDocumentStore)
             if obj_info['storeType'] == 'MODEL':
-                KodexaPlatform.__build_object(ref, object_type_metadata, RemoteModelStore)
+                return KodexaPlatform.__build_object(ref, object_type_metadata, RemoteModelStore)
 
         # TODO - there are other things we need?
         raise Exception(f"Unable to get a local instance of {ref} of type {object_type}")
