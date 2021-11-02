f34b6c61c60f8e543d73ff36d9b8e03e5659de09
Philip Dodds <philip@kodexa.ai>
2021-11-02 09:32:38 -0400

    Handling of values for selections where they can be Any not a Dict

--- generate-sources.sh
+++ generate-sources.sh
@@ -3,6 +3,7 @@
 datamodel-codegen --input resources/api-docs.yaml --output kodexa/model/objects.py --snake-case --base-class kodexa.model.base.KodexaBaseModel
 python -c "import sys;lines=sys.stdin.read();print(lines.replace('options: Optional[Dict[str, Dict[str, Any]]] = None','options: Optional[Dict[str, Any]] = None'))" < kodexa/model/objects.py > kodexa/model/objects_new.py
 
+cp kodexa/model/objects_new.py kodexa/model/objects.py
 
 # Handle value: Optional[Dict[str, Any]] = None - this is really an ANY
 python -c "import sys;lines=sys.stdin.read();print(lines.replace('value: Optional[Dict[str, Any]] = None','value: Optional[Any] = None'))" < kodexa/model/objects.py > kodexa/model/objects_new.py
