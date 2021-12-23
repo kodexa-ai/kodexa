e0fd69878d23b025695fd184c16fc98e08a0fb07
Philip Dodds <philip@kodexa.ai>
2021-12-23 07:55:26 -0500

    Make it easier to handle 4.0/4.1 conversions

--- kodexa/__init__.py
+++ kodexa/__init__.py
@@ -6,12 +6,13 @@ data.
 .. include:: ./documentation.md
 """
 from .assistant import Assistant, AssistantContext, AssistantResponse
-from .platform import KodexaPlatform, RemoteAction, RemotePipeline, RemoteSession
 from .connectors import FileHandleConnector, FolderConnector, UrlConnector, add_connector, get_connector, \
     get_connectors, get_source, registered_connectors
 from .model import ContentEvent, ContentFeature, ContentNode, Document, DocumentActor, DocumentFamily, DocumentMetadata, \
     DocumentStore, DocumentTransition, SourceMetadata, TransitionType
+from .model.objects import Taxonomy
 from .pipeline import Pipeline, PipelineContext, PipelineStatistics
+from .platform import KodexaPlatform, RemoteAction, RemotePipeline, RemoteSession
 from .steps import NodeTagCopy, NodeTagger, RollupTransformer, TagsToKeyValuePairExtractor, TextParser, \
     KodexaProcessingException
 from .stores import LocalDocumentStore, LocalModelStore, RemoteDocumentStore, \
