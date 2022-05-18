d78c7680db1cf35d266fa7ac709104e5add78ee5
Jack Dodds <jack@kodexa.com>
2022-05-18 17:33:35 -0400

    Code cleanup.

--- kodexa/stores/stores.py
+++ kodexa/stores/stores.py
@@ -206,11 +206,11 @@ class RemoteDataStore(Store):
             headers={"x-access-token": KodexaPlatform.get_access_token(), "content-type": "application/json"})
         if doc.status_code == 200:
             return
-        else:
-            logger.warning(
-                "Unable to post data objects to remote store [" + doc.text + "], response " + str(doc.status_code))
-            raise Exception(
-                "Unable to post data objects to remote store [" + doc.text + "], response " + str(doc.status_code))
+
+        logger.warning(
+            "Unable to post data objects to remote store [" + doc.text + "], response " + str(doc.status_code))
+        raise Exception(
+            "Unable to post data objects to remote store [" + doc.text + "], response " + str(doc.status_code))
 
     def add(self, row):
         from kodexa import KodexaPlatform
