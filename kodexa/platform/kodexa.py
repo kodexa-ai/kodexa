9a3eed883cc948b226ebc435335ac9fbaf929d8b
Philip Dodds <philip.dodds@me.com>
2022-05-25 10:35:00 -0400

    Fix up import

--- kodexa/platform/kodexa.py
+++ kodexa/platform/kodexa.py
@@ -24,10 +24,9 @@ from appdirs import AppDirs
 from functional import seq
 from rich import print, get_console
 
-from kodexa import DocumentStore
 from kodexa.connectors import get_source
 from kodexa.connectors.connectors import get_caller_dir, FolderConnector
-from kodexa.model import Document, ExtensionPack, ModelStore
+from kodexa.model import Document, ExtensionPack, ModelStore, DocumentStore
 from kodexa.model.objects import AssistantDefinition, Action, Taxonomy, ModelRuntime, Credential, ExecutionEvent, \
     ContentObject, AssistantEvent, ContentEvent, ScheduledEvent, Project, Execution, ProjectTemplate, Membership
 from kodexa.pipeline import PipelineContext, Pipeline, PipelineStatistics
