67c56005abfba2b0d7a0bf3545b4428d64bec7cc
Philip Dodds <philip.dodds@me.com>
2022-10-14 05:50:53 -0400

    Include a training name so we can remember things

--- kodexa/model/objects.py
+++ kodexa/model/objects.py
@@ -988,6 +988,7 @@ class UserActivation(KodexaBaseModel):
 class ModelTraining(KodexaBaseModel):
     id: Optional[str] = Field(None, description='The ID of the object')
     uuid: Optional[str] = None
+    name: Optional[str] = None
     created_on: Optional[datetime] = Field(None, alias='createdOn')
     updated_on: Optional[datetime] = Field(None, alias='updatedOn')
     training_date: Optional[datetime] = Field(None, alias='trainingDate')
