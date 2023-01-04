6d109ac611d9618bd8d3642bc024bc7d44908be2
Philip Dodds <philip.dodds@me.com>
2023-01-04 11:43:51 -0500

    Change remove to discard

--- kodexa/model/persistence.py
+++ kodexa/model/persistence.py
@@ -774,7 +774,7 @@ class PersistenceManager(object):
         self.node_cache.remove_obj(node)
 
         if node.uuid in self.node_parent_cache:
-            self.child_cache[self.node_parent_cache[node.uuid]].remove(node)
+            self.child_cache[self.node_parent_cache[node.uuid]].discard(node)
 
             # We have a sitation where we seem to fail here?
             self.child_id_cache[self.node_parent_cache[node.uuid]].discard(node.uuid)
