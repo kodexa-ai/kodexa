e552f6f8b7bc6bd56d561e578c0435f680140890
Philip Dodds <philip.dodds@me.com>
2023-08-13 16:34:10 -0400

    Try to clean up

--- kodexa/testing/__init__.py
+++ kodexa/testing/__init__.py
@@ -2,6 +2,6 @@
 Utilities to help support unit testing and test harnesses for Kodexa
 """
 
-from .test_components import TestAction, TestAssistant
-from .test_utils import AssistantTestHarness, compare_document, DocumentTestCaptureStep, ExceptionBuilder, \
+from test_components import TestAction, TestAssistant
+from test_utils import AssistantTestHarness, compare_document, DocumentTestCaptureStep, ExceptionBuilder, \
     ExtensionPackUtil, OptionException, simplify_node
