43c3b4a81b80427952bcf7c5d40bdc13f8855a0f
Frank Wanicka <frank@kodexa.com>
2025-03-24 13:57:57 -0400

    Fixed serialization bug with Task.update_status, TaskEvent tweaks

--- kodexa/platform/client.py
+++ kodexa/platform/client.py
@@ -1241,7 +1241,7 @@ class TaskEndpoint(EntityEndpoint, Task):
     def update_status(self, status: TaskStatus):
         """Update the status of the task."""
         url = f"/api/tasks/{self.id}/status"
-        response = self.client.put(url, body=status)
+        response = self.client.put(url, body=status.model_dump(mode="json", by_alias=True))
         return TaskEndpoint.model_validate(response.json()).set_client(self.client)
 
     def remove_status(self):
